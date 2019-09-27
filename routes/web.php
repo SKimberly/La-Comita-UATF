367546ead726c497f1383dc475e08f125a605ac2
SKimberly <kimi.uatf@gmail.com>
2019-09-26 23:43:18 -0400

    Instale auth npm adminlte fontawe sweetaler finish

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('redirect', function(){
+	//alert()->error('Success Message', 'Optional Title');
+	return redirect('/home')->with('success', 'Bienvenido!');
+});
